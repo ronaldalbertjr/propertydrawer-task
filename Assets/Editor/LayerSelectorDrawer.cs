3f046f3ac06cc0d6e107c86b55270a27421665f3
ronaldalbertjr <ronaldalbert1609@gmail.com>
2017-09-01 10:36:55 -0300

    FixedVariableTypeBug
    Fixed a bug there would be an error if someone used the drawer with a wrong variable type

--- Assets/Editor/LayerSelectorDrawer.cs
+++ Assets/Editor/LayerSelectorDrawer.cs
@@ -10,25 +10,26 @@ public class LayerSelectorDrawer : PropertyDrawer
 {
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        Rect popupRect = new Rect(position.x, position.y, position.width, position.height);
-
-
-        int index = System.Array.IndexOf(InternalEditorUtility.layers, LayerMask.LayerToName(property.intValue));
-        var options = InternalEditorUtility.layers.ToList();
-        options.Add("");
-        options.Add("New Layer");
-        index = EditorGUI.Popup(popupRect, "Layers: ", index, options.ToArray());
-
-
-        if( index == options.Count - 1)
+        if (property.propertyType == SerializedPropertyType.Integer)
         {
-            var a = AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0];
-            Selection.activeObject = a;
-        }
-        else
-        {
-            string layerName = InternalEditorUtility.layers[index];
-            property.intValue = LayerMask.NameToLayer(layerName);
+            Rect popupRect = new Rect(position.x, position.y, position.width, position.height);
+            int index = System.Array.IndexOf(InternalEditorUtility.layers, LayerMask.LayerToName(property.intValue));
+            var options = InternalEditorUtility.layers.ToList();
+            options.Add("");
+            options.Add("New Layer");
+            index = EditorGUI.Popup(popupRect, "Layers: ", index, options.ToArray());
+
+
+            if (index == options.Count - 1)
+            {
+                var a = AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0];
+                Selection.activeObject = a;
+            }
+            else
+            {
+                string layerName = InternalEditorUtility.layers[index];
+                property.intValue = LayerMask.NameToLayer(layerName);
+            }
         }
     }
 }
--- Assets/Editor/TagSelectorDrawer.cs
+++ Assets/Editor/TagSelectorDrawer.cs
@@ -10,17 +10,20 @@ public class TagSelectorDrawer :  PropertyDrawer
     int index;
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        for(int i = 0; i < UnityEditorInternal.InternalEditorUtility.tags.Length; i++)
+        if (property.propertyType == SerializedPropertyType.String)
         {
-            if(UnityEditorInternal.InternalEditorUtility.tags[i].Equals(property.stringValue))
+            for (int i = 0; i < UnityEditorInternal.InternalEditorUtility.tags.Length; i++)
             {
-                index = i;
+                if (UnityEditorInternal.InternalEditorUtility.tags[i].Equals(property.stringValue))
+                {
+                    index = i;
+                }
             }
+
+
+            Rect popupRect = new Rect(position.x, position.y, position.width, position.height);
+            index = EditorGUI.Popup(popupRect, "Tags: ", index, InternalEditorUtility.tags);
+            property.stringValue = UnityEditorInternal.InternalEditorUtility.tags[index];
         }
-
-
-        Rect popupRect = new Rect(position.x, position.y, position.width, position.height);
-        index = EditorGUI.Popup(popupRect,"Tags: ", index, InternalEditorUtility.tags);
-        property.stringValue = UnityEditorInternal.InternalEditorUtility.tags[index];
     }
 }
