c2f0e0b33ffa56b565c650dbee44106fbf54e680
ronaldalbertjr <ronaldalbert1609@gmail.com>
2017-09-01 10:22:54 -0300

    TagsAndLayers
    System using property drawer to make an editable layer and tag changer in a gameobject

---
+++ Assets/Editor/LayerSelectorDrawer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+using UnityEditorInternal;
+
+[CustomPropertyDrawer(typeof(LayerSelectorAttribute))]
+public class LayerSelectorDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        Rect popupRect = new Rect(position.x, position.y, position.width, position.height);
+
+
+        int index = System.Array.IndexOf(InternalEditorUtility.layers, LayerMask.LayerToName(property.intValue));
+        var options = InternalEditorUtility.layers.ToList();
+        options.Add("");
+        options.Add("New Layer");
+        index = EditorGUI.Popup(popupRect, "Layers: ", index, options.ToArray());
+
+
+        if( index == options.Count - 1)
+        {
+            var a = AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0];
+            Selection.activeObject = a;
+        }
+        else
+        {
+            string layerName = InternalEditorUtility.layers[index];
+            property.intValue = LayerMask.NameToLayer(layerName);
+        }
+    }
+}
---
+++ Assets/Script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Script : MonoBehaviour
+{
+    [TagSelector]
+    public string MyTag;
+
+    [LayerSelector]
+    public int MyLayer;
+
+    private void Update()
+    {
+        gameObject.tag = MyTag;
+        gameObject.layer = MyLayer;
+        
+    }
+}
